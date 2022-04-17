47369d9b7a804a4903b51d9f07e65e0fa96652f1
jason <wei1396981310@163.com>
2022-04-17 19:53:28 +0800

    拆分好了Home页面，基本配置好了路由

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,49 @@
+<!-- 主页 -->
+<template>
+  <div>
+    <Nav-type></Nav-type>
+    <Swiper></Swiper>
+    <Recommend></Recommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Floor></Floor>
+    <Brand></Brand>
+  </div>
+</template>
+
+<script>
+import Swiper from "@/pages/Home/Swiper"
+import Like from "@/pages/Home/Like"
+import Rank from "@/pages/Home/Rank"
+import Floor from "@/pages/Home/Floor"
+import Recommend from "@/pages/Home/Recommend"
+import Brand from "@/pages/Home/Brand"
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    Swiper,
+    Like,
+    Rank,
+    Floor,
+    Recommend,
+    Brand
+  },
+  //生命周期 - 创建完成（访问当前this实例）
+  created () {
+
+  },
+  //生命周期 - 挂载完成（访问DOM元素）
+  mounted () {
+
+  }
+}
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+</style>
