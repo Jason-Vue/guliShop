96f870d196a0bcc03375a65a2601d2a6e679120d
jason <wei1396981310@163.com>
2022-05-18 18:10:39 +0800

    完成了floor开发,就是为由给轮播做封装

--- src/api/index.js
+++ src/api/index.js
@@ -13,3 +13,6 @@ export const reqCategoryList = () => {
 
 // 2.首页轮播图的资源
 export const reqBannerList = () => mockRequest.get('/banner')
+
+// 3.首页floor的资源
+export const reqFloorList = () => mockRequest.get('/floor')
--- src/pages/Home/Floor/index.vue
+++ src/pages/Home/Floor/index.vue
@@ -4,36 +4,14 @@
     <div class="floor">
       <div class="py-container">
         <div class="title clearfix">
-          <h3 class="fl">家用电器</h3>
+          <h3 class="fl">{{floor.name}}</h3>
           <div class="fr">
             <ul class="nav-tabs clearfix">
-              <li class="active">
-                <a href="#tab1"
-                   data-toggle="tab">热门</a>
-              </li>
-              <li>
-                <a href="#tab2"
-                   data-toggle="tab">大家电</a>
-              </li>
-              <li>
-                <a href="#tab3"
-                   data-toggle="tab">生活电器</a>
-              </li>
-              <li>
-                <a href="#tab4"
-                   data-toggle="tab">厨房电器</a>
-              </li>
-              <li>
-                <a href="#tab5"
-                   data-toggle="tab">应季电器</a>
-              </li>
-              <li>
-                <a href="#tab6"
-                   data-toggle="tab">空气/净水</a>
-              </li>
-              <li>
-                <a href="#tab7"
-                   data-toggle="tab">高端电器</a>
+              <li class="active"
+                  v-for="(list,index) in floor.navList"
+                  :key="index">
+                <a :href="list.url"
+                   data-toggle="tab">{{list.text}}</a>
               </li>
             </ul>
           </div>
@@ -43,21 +21,19 @@
             <div class="floor-1">
               <div class="blockgary">
                 <ul class="jd-list">
-                  <li>节能补贴</li>
-                  <li>4K电视</li>
-                  <li>空气净化器</li>
-                  <li>IH电饭煲</li>
-                  <li>滚筒洗衣机</li>
-                  <li>电热水器</li>
+                  <li v-for="(kw,index) in floor.keywords"
+                      :key="index">{{kw}}</li>
                 </ul>
-                <img src="./images/floor-1-1.png" />
+                <img :src="floor.imgUrl" />
               </div>
               <div class="floorBanner">
                 <div class="swiper-container"
-                     id="floor1Swiper">
+                     ref="floorSwiper">
                   <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                      <img src="./images/floor-1-b01.png">
+                    <div class="swiper-slide"
+                         v-for="(carousel,index) in floor.carouselList"
+                         :key="carousel.id">
+                      <img :src="carousel.imageUrl">
                     </div>
                   </div>
                   <!-- 如果需要分页器 -->
@@ -71,22 +47,22 @@
               <div class="split">
                 <span class="floor-x-line"></span>
                 <div class="floor-conver-pit">
-                  <img src="./images/floor-1-2.png" />
+                  <img :src="floor.recommendList[0]" />
                 </div>
                 <div class="floor-conver-pit">
-                  <img src="./images/floor-1-3.png" />
+                  <img :src="floor.recommendList[1]" />
                 </div>
               </div>
               <div class="split center">
-                <img src="./images/floor-1-4.png" />
+                <img :src="floor.bigImg" />
               </div>
               <div class="split">
                 <span class="floor-x-line"></span>
                 <div class="floor-conver-pit">
-                  <img src="./images/floor-1-5.png" />
+                  <img :src="floor.recommendList[2]" />
                 </div>
                 <div class="floor-conver-pit">
-                  <img src="./images/floor-1-6.png" />
+                  <img :src="floor.recommendList[3]" />
                 </div>
               </div>
             </div>
@@ -98,10 +74,18 @@
 </template>
 
 <script>
+import Swiper from "swiper"
 export default {
   data () {
     return {
 
+    }
+  },
+  props: {
+    floor: {
+      type: Object,
+      default: {},
+      require: true
     }
   },
   //生命周期 - 创建完成（访问当前this实例）
@@ -110,7 +94,32 @@ export default {
   },
   //生命周期 - 挂载完成（访问DOM元素）
   mounted () {
+    /* 
+    这里的配置new Swiper不需要使用watch+$nextTick(()=>{})是因为，这里的数据并不是异步请求的，
+    而是由Home的index页面通过props方法传给floor组件的，所以数据是一定会拿到的，所以就不会出问题
+    */
+    var mySwiper = new Swiper(this.$refs.floorSwiper, {
+      // direction: 'vertical', // 垂直切换选项
+      loop: true, // 循环模式选项
+      autoplay: {
+        delay: 2000
+      },
+      // 如果需要分页器
+      pagination: {
+        el: '.swiper-pagination',
+      },
 
+      // 如果需要前进后退按钮
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      // 如果需要滚动条
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    })
   }
 }
 </script>
