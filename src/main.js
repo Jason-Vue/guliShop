0f6532871490ee560794462962199ffc9dd68177
jason <wei1396981310@163.com>
2023-08-08 18:29:56 +0800

    切换远程地址到github之前提交

--- src/main.js
+++ src/main.js
@@ -1,33 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 // 引入路由
-import router from "@/router/index"
-import store from "@/store/index.js"
+import router from "@/router/index";
+import store from "@/store/index.js";
 
 // 引入element-ui
-import "@/utils/element"
-
+import "@/utils/element";
 
 // 全局组件
-import NavType from "@/components/NavType"
-import Pagination from "@/components/Pagination"
-Vue.component('Nav-type', NavType)
-Vue.component('Pagination', Pagination)
+import NavType from "@/components/NavType";
+import Pagination from "@/components/Pagination";
+Vue.component("Nav-type", NavType);
+Vue.component("Pagination", Pagination);
 
 // 引入mock.js
-import "@/mock/mockServer"
+import "@/mock/mockServer";
 // 引入首页轮播图样式
-import "swiper/css/swiper.css"
+import "swiper/css/swiper.css";
 
 // 统一接口API文件夹里面的全部请求函数
-import * as API from "@/api/index.js"
+import * as API from "@/api/index.js";
 
 // 使用懒加载
-import VueLazyload from 'vue-lazyload'
-import time from "@/assets/timg.png"
+import VueLazyload from "vue-lazyload";
+import time from "@/assets/timg.png";
 Vue.use(VueLazyload, {
-  loading: time
-})
+  loading: time,
+});
 
 // import "@/api/test"
 // import {
@@ -35,14 +34,14 @@ Vue.use(VueLazyload, {
 // } from "@/api/index"
 // console.log(reqSearchList());
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
   router: router,
   store: store,
   beforeCreate() {
     Vue.prototype.$bus = this;
-    Vue.prototype.$API = API
-  }
-}).$mount('#app')
+    Vue.prototype.$API = API;
+  },
+}).$mount("#app");
