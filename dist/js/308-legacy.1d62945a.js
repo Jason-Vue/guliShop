ba3e9bf28518457cb974ff6e8ace84ec29fe0705
jason <wei1396981310@163.com>
2023-08-08 19:17:58 +0800

    项目打包上去,dist

---
+++ dist/js/308-legacy.1d62945a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmy_gulishop"]=self["webpackChunkmy_gulishop"]||[]).push([[308],{78308:function(n,e,t){t.r(e),t.d(e,{default:function(){return f}});var u=function(){var n=this,e=n.$createElement;n._self._c;return n._m(0)},r=[function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[t("h1",[n._v("团购订单")])])}],c={data:function(){return{}},methods:{},created:function(){},mounted:function(){}},l=c,i=t(43736),s=(0,i.Z)(l,u,r,!1,null,null,null),f=s.exports}}]);
+//# sourceMappingURL=308-legacy.1d62945a.js.map
