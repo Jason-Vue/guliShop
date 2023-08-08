fd2c463a016d343a91f27870b81e2dc1094c636f
jason <wei1396981310@163.com>
2023-08-08 20:32:03 +0800

    创建gh-pages分支,实现github预览

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,13 @@ const {
 } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+  //基本路径
+  publicPath: './',//默认的'/'是绝对路径，如果不确定在根路径，改成相对路径'./'
+  // 输出文件目录
+  outputDir: 'dist',
+  assetsDir: 'src',
+  indexPath: 'index.html',
+
   transpileDependencies: true,
   // 关闭Eslint
   lintOnSave: false,
